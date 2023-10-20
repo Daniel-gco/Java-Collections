2c5188afa94f28f3303829f7ecca4f010e6be47d
Daniel-gco <ro.oli3465@gmail.com>
2023-10-20 20:58:49 -0300

    Finalizando list

--- src/collections/list/ordenacao/ListaPessoa.java
+++ src/collections/list/ordenacao/ListaPessoa.java
@@ -1,4 +1,29 @@
 package collections.list.ordenacao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ListaPessoa {
+    private List<Pessoa> listaPessoas;
+
+    public ListaPessoa() {
+        this.listaPessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        listaPessoas.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> listaPorIdade = new ArrayList<>(listaPessoas);
+        Collections.sort(listaPorIdade);
+        return listaPorIdade;
+    }
+
+    public List<Pessoa> ordenaPorAltura() {
+        List<Pessoa> listaPorAltura = new ArrayList<>(listaPessoas);
+        Collections.sort(listaPorAltura, new ComparatorPorAltura() );
+        return listaPorAltura;
+    }
 }
